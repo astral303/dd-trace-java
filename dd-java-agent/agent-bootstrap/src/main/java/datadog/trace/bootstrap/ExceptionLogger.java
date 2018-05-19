08151df342be61f807a9a50ce5661e7bca016b1b
Andrew Kent <andrew.kent@datadoghq.com>
2018-05-18 17:16:53 -0700

    Akka-http instrumentation and test

--- dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/ExceptionLogger.java
+++ dd-java-agent/agent-bootstrap/src/main/java/datadog/trace/bootstrap/ExceptionLogger.java
@@ -1,8 +1,15 @@
 package datadog.trace.bootstrap;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Class used for exception handler logging.
  *
  * <p>See datadog.trace.agent.tooling.ExceptionHandlers
  */
-public class ExceptionLogger {}
+@Slf4j
+public class ExceptionLogger {
+  public static void logThrowable(Throwable t) {
+    log.debug("Failed to handle exception in instrumentation", t);
+  }
+}
